cf1bc1cc2c5dd29cadf4f4552e16fbbb3209be04
rhysinku <rhysinvillahermosa16@gmail.com>
2025-01-31 10:45:19 +0800

    add Onchange with visualization on JSON

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -10,20 +10,28 @@ function Calculator() {
         setInput([...input, { name: newInputName, value: 0 }]);
     }
 
+    const handleInputChange = (index: number, field: keyof typeof input[0], value: string | number) => {
+        const updatedInput = [...input]; // Create a copy of the state
+        updatedInput[index][field] = value as never; // Update the specific field
+        setInput(updatedInput); // Update the state
+    };
+
     return (
         <>
             <div className={'mb-5'}>
                 {input.map((item, index) =>
                     <div key={index} >
                         <input type="text"
-                               name={item.name}
+                               name={item.name.toString()}
                                value={item.name}
+                               onChange={(e) => handleInputChange(index, 'name', e.target.value)}
                         />
                         <input
-                            name={item.name}
+                            name={item.name.toString()}
                             type="text"
                             value={item.value}
                             className={'border-secondary border-1 border-solid rounded py-1 px-2 bg-white text-primary'}
+                            onChange={(e) => handleInputChange(index, 'value', parseFloat(e.target.value))}
                         />
                     </div>
                 )}
@@ -32,6 +40,10 @@ function Calculator() {
             <section>
                 <button onClick={addInput} className={'py-1 px-2 bg-quaternary text-white rounded'}> Add Data</button>
             </section>
+
+            <code className={'py-1 px-2 bg-quaternary rounded mt-5'}>
+                <pre>{JSON.stringify(input, null, 2)}</pre>
+            </code>
         </>
     )
         ;
