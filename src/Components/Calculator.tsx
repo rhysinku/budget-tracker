cfd1153280d4a42b542cc49b3651015128ae3e56
rhysinku <rhysinvillahermosa16@gmail.com>
2025-01-31 17:09:11 +0800

    up

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -2,12 +2,14 @@ import {useState} from "react";
 
 function Calculator() {
     const [input, setInput] = useState<{ name: string, value: number } []>([
-        {name: 'Salary', value: 0}
+        {name: 'Allowance', value: 0}
     ])
 
-    const addInput =() =>{
+    const [salary, setSalary] = useState<number>(0)
+
+    const addInput = () => {
         const newInputName = `input${input.length + 1}`;
-        setInput([...input, { name: newInputName, value: 0 }]);
+        setInput([...input, {name: newInputName, value: 0}]);
     }
 
     const handleInputChange = (index: number, field: keyof typeof input[0], value: string | number) => {
@@ -18,32 +20,43 @@ function Calculator() {
 
     return (
         <>
-            <div className={'mb-5'}>
-                {input.map((item, index) =>
-                    <div key={index} >
-                        <input type="text"
+           <div className={'container'}>
+               <section className={'flex justify-between items-center w-full'}>
+                   <label htmlFor="salary">Salary</label>
+                   <input type="text"
+                          name={'salary'}
+                          placeholder={'Salary'}
+                          value={salary}
+                          className={'border-secondary border-1 border-solid rounded py-1 px-2 bg-white text-primary'}
+                          onChange={(e) => setSalary(parseFloat(e.target.value))}
+                   />
+               </section>
+               <section className={'mb-5 bg-quaternary py-2 px-2'}>
+                   {input.map((item, index) =>
+                       <div key={index} className={'w-full flex justify-between items-center'}>
+                           <input type="text"
+                                  name={item.name.toString()}
+                                  value={item.name}
+                                  onChange={(e) => handleInputChange(index, 'name', e.target.value)}
+                           />
+                           <input
                                name={item.name.toString()}
-                               value={item.name}
-                               onChange={(e) => handleInputChange(index, 'name', e.target.value)}
-                        />
-                        <input
-                            name={item.name.toString()}
-                            type="text"
-                            value={item.value}
-                            className={'border-secondary border-1 border-solid rounded py-1 px-2 bg-white text-primary'}
-                            onChange={(e) => handleInputChange(index, 'value', parseFloat(e.target.value))}
-                        />
-                    </div>
-                )}
-            </div>
-
-            <section>
-                <button onClick={addInput} className={'py-1 px-2 bg-quaternary text-white rounded'}> Add Data</button>
-            </section>
-
-            <code className={'py-1 px-2 bg-quaternary rounded mt-5'}>
-                <pre>{JSON.stringify(input, null, 2)}</pre>
-            </code>
+                               type="text"
+                               value={item.value}
+                               className={'border-secondary border-1 border-solid rounded py-1 px-2 bg-white text-primary'}
+                               onChange={(e) => handleInputChange(index, 'value', parseFloat(e.target.value))}
+                           />
+                       </div>
+                   )}
+                   <div>
+                       <button onClick={addInput} className={'py-1 px-2 bg-secondary text-white rounded'}> Add Data
+                       </button>
+                   </div>
+               </section>
+               <code className={'py-1 px-2 bg-quaternary rounded mt-5 block'}>
+                   <pre>{JSON.stringify(input, null, 2)}</pre>
+               </code>
+           </div>
         </>
     )
         ;
