24c9afc4a7667e504e38041a1d14b028880697cf
rhysinku <rhysinvillahermosa16@gmail.com>
2025-02-15 17:41:01 +0800

    add shadcdn

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,10 +1,29 @@
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table.tsx";
+
 function Calculator() {
 
     return (
         <>
-            <div>
-                HELLO
-            </div>
+         <div className={'container mx-auto'}>
+             <Table>
+                 <TableHeader>
+                     <TableRow>
+                         <TableHead>Name</TableHead>
+                         <TableHead>Value</TableHead>
+                     </TableRow>
+                 </TableHeader>
+                 <TableBody>
+                     <TableRow>
+                         <TableCell>
+                             Bills
+                         </TableCell>
+                         <TableCell>
+                             1000
+                         </TableCell>
+                     </TableRow>
+                 </TableBody>
+             </Table>
+         </div>
         </>
     );
 }
